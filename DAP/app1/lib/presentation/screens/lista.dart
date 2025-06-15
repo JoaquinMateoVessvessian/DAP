a35cc46b38f733f31c4598370169309cdef3794f
JoaquinMateoVessvessian <joaquinvessvessian21@gmail.com>
2025-06-15 15:33:00 -0300

    esga

--- DAP/app1/lib/presentation/screens/lista.dart
+++ DAP/app1/lib/presentation/screens/lista.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Lista extends StatelessWidget {
+class Lista extends StatefulWidget {
   final String title;
   final String photo;
   final String description;
-
   const Lista({
     super.key,
     required this.title,
     required this.photo,
     required this.description,
   });
+  @override
+  State<Lista> createState() => _ListaState();
+}
+
+class _ListaState extends State<Lista> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _photoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Column(
-        children: [
-          Image.network(photo),
-          Text(description),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Guardar'),
-          ),
-        ],
+      appBar: AppBar(title: const Text('add Grocerie')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(labelText: 'title'),
+            ),
+            TextField(
+              controller: _descriptionController,
+              decoration: const InputDecoration(labelText: 'description'),
+            ),
+            TextField(
+              controller: _photoController,
+              decoration: const InputDecoration(labelText: 'photo URL'),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/homescreen');
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
       ),
     );
   }
